ad702fabd5acabef5fc04f57a95cb87ee7c3ebde
YADOQ <421108010@qu.edu.sa>
2025-03-09 14:18:57 +0300

    update3

--- Python_Banking_App/py/banking.py
+++ Python_Banking_App/py/banking.py
@@ -12,30 +12,36 @@
 import csv
 
 class Bank_System():
-    def __init__(self, name, cheacking_account = [], savings_account = [], active_account = [], balance = 0):
+    def __init__(self, name, cheacking_account = [], savings_account = [], balance = 0):
+        with open('./../bank.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                print(row)
         self.name = name
         self.cheacking_account = cheacking_account
         self.savings_account = savings_account
         self.balance = balance
-        with open('bank.csv', 'r') as file:
-            file = csv.reader(file)
-            for row in file:
-                print(row)
+         
 
     def add_new_customer(self):
-        self.name = input('Enter your name: ')
-        inp = input('Do you have a cheacking account? ')
-        inp2 = input('Do you have a savings account? ')
+        self.name = input('Enter your name: ').lower()
+        inp = input('Do you have a cheacking account? ').lower()
+        inp2 = input('Do you have a savings account? ').lower()
         if inp == 'yes':
             self.cheacking_account.append(self.name)
+        else:
+            print('You do not have a cheacking account')
         if inp2 == 'yes':
             self.savings_account.append(self.name)
-        if inp == 'yes' and inp2 == 'yes':
-            self.cheacking_account.append(self.name)
-            self.savings_account.append(self.name)
-        with open('bank.csv', 'a') as file:
-            file = csv.writer(file)
-            file.writerow([self.name, self.cheacking_account, self.savings_account])
+        else:
+            print('You do not have a savings account')
+        
+        with open('./../bank.csv', 'a', newline='') as file:
+            feidnames = ['Name', 'Cheacking Account', 'Savings Account']
+            writer = csv.DictWriter(file, fieldnames=feidnames)
+            headers = writer.fieldnames()
+            if not headers:
+                writer.writeheader()
         return self.name, self.cheacking_account, self.savings_account
         
         
@@ -43,14 +49,20 @@ class Bank_System():
         
     def withdraw_money(self, balance):
         self.balance = balance
+        inp = input('How much would you like to withdraw? ')
+        self.balance -= int(inp)
         with open('bank.csv', 'a') as file:
             file = csv.writer(file)
             file.writerow([self.balance])
         return self.balance
         
 
-    def deposit_money():
-        pass
+    def deposit_money(self, balance):
+        self.balance = balance
+        inp = input('How much would you like to deposit? ')
+        self.balance += int(inp)
+        return self.balance
+
 
 
     def transfer_money():
@@ -59,3 +71,8 @@ class Bank_System():
 
     def overdraft():
         pass
+    
+
+Bank_System('name').add_new_customer()
+Bank_System('name').withdraw_money(100)
+Bank_System('name').deposit_money(100)

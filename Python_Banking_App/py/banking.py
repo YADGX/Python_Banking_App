ea35d5691760ca50976e21925eb80e8a51303a7b
YADOQ <421108010@qu.edu.sa>
2025-03-09 10:38:13 +0300

    Starting Point

---
+++ Python_Banking_App/py/banking.py
@@ -0,0 +1,19 @@
+import csv
+
+
+def add_new_customer():
+    pass
+    
+def withdraw_money():
+    pass
+
+def deposit_money():
+    pass
+
+
+def transfer_money():
+    pass
+
+
+def overdraft():
+    pass
